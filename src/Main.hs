0b151f61962b6fdcbcd03d56269d49a6bc176f03
Fernando Freire <dogonthehorizon@gmail.com>
2021-05-22 10:01:37 -0700

    feat(pacrd): add models for pacrd parsing

--- src/Main.hs
+++ src/Main.hs
@@ -1,5 +1,32 @@
 module Main where
 
+import           Control.Applicative ((<|>))
+import           Control.Monad       (forM_)
+import qualified Data.List           as List
+import           Data.Yaml           (FromJSON)
+import qualified Data.Yaml           as Yaml
+import qualified Pacrd
+import qualified System.Directory    as Dir
+
+-- | Read a manifest file from the given path.
+readManifest :: (FromJSON a) => FilePath -> IO a
+readManifest path = do
+    result <- Yaml.decodeFileEither path
+    case result of
+        Left  err -> fail $ "could not read '" ++ path ++ "': " ++ show err
+        Right val -> return val
+
+readPipeline :: FilePath -> IO Pacrd.Pipeline
+readPipeline = readManifest
+
+readApp :: FilePath -> IO Pacrd.Application
+readApp = readManifest
+
+isYamlFile :: FilePath -> Bool
+isYamlFile path = List.isSuffixOf ".yaml" path || List.isSuffixOf ".yml" path
+
 main :: IO ()
 main = do
-  putStrLn "hello world"
+    -- TODO support input paths
+    files <- filter isYamlFile <$> Dir.listDirectory "."
+    forM_ files $ \f -> (readPipeline f >>= print) <|> (readApp f >>= print)
---
+++ src/Pacrd.hs
@@ -0,0 +1,172 @@
+{-|
+Module : Pacrd
+Description : Types and operations in the PaCRD domain.
+Copyright : (c) Armory.io 2021
+License : BSD3
+Maintainer : Fernando Freire
+Stability : experimental
+-}
+
+{-# LANGUAGE DuplicateRecordFields #-}
+
+module Pacrd where
+
+import Data.Text    (Text)
+import Data.Yaml    (FromJSON, Value (String), parseJSON, withObject, (.:))
+import GHC.Generics (Generic)
+
+-- | Represents a PaCRD Application manifest.
+data Application = Application {
+    apiVersion :: !Text,
+    kind       :: !Kind,
+    metadata   :: !Metadata,
+    spec       :: !ApplicationSpec
+  } deriving (Show, Generic)
+
+instance FromJSON Application
+
+data ApplicationSpec = ApplicationSpec {
+    email       :: !(Maybe Text),
+    description :: !(Maybe Text),
+    dataSources :: !(Maybe DataSources),
+    permissions :: !(Maybe Permissions)
+  } deriving (Show, Generic)
+
+instance FromJSON ApplicationSpec
+
+data Permissions = Permissions {
+    read    :: !(Maybe [Text]),
+    write   :: !(Maybe [Text]),
+    execute :: !(Maybe [Text])
+  } deriving (Show)
+
+instance FromJSON Permissions where
+  parseJSON = withObject "Permissions" $ \v ->
+    Permissions
+    <$> v .: "READ"
+    <*> v .: "WRITE"
+    <*> v .: "EXECUTE"
+
+data DataSources = DataSources {
+    enabled  :: !(Maybe [DataSource]),
+    disabled :: !(Maybe [DataSource])
+  } deriving (Show, Generic)
+
+instance FromJSON DataSources
+
+data DataSource =
+  ServerGroup | Execution | LoadBalancer | SecurityGroup
+  deriving (Show)
+
+instance FromJSON DataSource where
+  parseJSON (String s) =
+    case s of
+      "serverGroups"   -> pure ServerGroup
+      "executions"     -> pure Execution
+      "loadBalancer"   -> pure LoadBalancer
+      "securityGroups" -> pure SecurityGroup
+      _                -> fail "not a valid data source"
+  parseJSON _ = fail "not a string"
+
+data Kind = KindPipeline | KindApplication deriving (Show)
+
+instance FromJSON Kind where
+  parseJSON (String s) =
+    case s of
+      "Pipeline"    -> pure KindPipeline
+      "Application" -> pure KindApplication
+      _             -> fail "not a supported kind"
+  parseJSON _ = fail "not a string"
+
+-- | Represents a PaCRD Pipeline manifest.
+data Pipeline = Pipeline {
+  apiVersion :: !Text,
+  metadata   :: !Metadata,
+  kind       :: !Kind,
+  spec       :: !PipelineSpec
+} deriving (Show, Generic)
+
+instance FromJSON Pipeline
+
+newtype Metadata = Metadata {
+    name :: Text
+  } deriving (Show, Generic)
+
+instance FromJSON Metadata
+
+data PipelineSpec = PipelineSpec {
+    application             :: !Text,
+    description             :: !(Maybe Text),
+    parameterConfig         :: !(Maybe [Parameter]),
+    expectedArtifacts       :: !(Maybe [Artifact]),
+    executionEngine         :: !(Maybe Text),
+    allowParallelExecutions :: !(Maybe Bool),
+    limitConcurrent         :: !(Maybe Bool),
+    keepWaitingPipelines    :: !(Maybe Bool),
+    stages                  :: ![Stage],
+    triggers                :: !(Maybe [Trigger])
+  } deriving (Show, Generic)
+
+instance FromJSON PipelineSpec
+
+data Stage = Stage {
+    type'      :: !Text,
+    properties :: !Value
+    -- ^ Bag of properties. We can't get more specific here without re-implementing PaCRD.
+  } deriving (Show)
+
+instance FromJSON Stage where
+  parseJSON = withObject "Stage" $ \o ->
+    Stage <$> o .: "type" <*> o .: "properties"
+
+data Trigger = Trigger {
+    type'      :: !Text,
+    properties :: !Value
+    -- ^ Bag of properties. We can't get more specific here without re-implementing PaCRD.
+  } deriving (Show)
+
+instance FromJSON Trigger where
+  parseJSON = withObject "Trigger" $ \o ->
+    Trigger <$> o .: "type" <*> o .: "properties"
+
+data Parameter = Parameter {
+    default'    :: !Text,
+    description :: !Text,
+    hasOptions  :: !Bool,
+    label       :: !Text,
+    name        :: !Text,
+    options     :: !Option,
+    pinned      :: !Bool,
+    required    :: !Bool
+  } deriving (Show, Generic)
+
+instance FromJSON Parameter
+
+newtype Option = Option {
+    value :: Text
+  } deriving (Show, Generic)
+
+instance FromJSON Option
+
+data Artifact = Artifact {
+    id                 :: !Text,
+    displayName        :: !Text,
+    usePriorArtifact   :: !(Maybe Bool),
+    useDefaultArtifact :: !(Maybe Bool),
+    defaultArtifact    :: !(Maybe MatchArtifact),
+    matchArtifact      :: !(Maybe MatchArtifact)
+  } deriving (Show, Generic)
+
+instance FromJSON Artifact
+
+data MatchArtifact = MatchArtifact {
+    type'      :: !Text,
+    properties :: !Value
+    -- ^ Bag of properties. We can't get more specific here without re-implementing PaCRD.
+  } deriving (Show, Generic)
+
+instance FromJSON MatchArtifact where
+  parseJSON = withObject "MatchArtifact" $ \o ->
+    MatchArtifact
+    <$> o .: "type"
+    <*> o .: "properties"
